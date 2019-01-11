9831d1c00d9ec0d9df3f74d485c57b5741661bd9
Stefano <stefano0012@gmail.com>
2019-01-11 17:45:27 +0100

    add link for CTA 'Do you want to create your workout?'

--- src/Controller/WorkoutController.php
+++ src/Controller/WorkoutController.php
@@ -2,17 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Exercise;
 use App\Entity\Workout;
 use App\Form\WorkoutFormType;
 use App\Repository\WorkoutRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
